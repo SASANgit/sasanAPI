146b59b6263cc7d9f362f50759d3f756130c0038
SASANgit ** MrLTE <44064519+SASANgit@users.noreply.github.com>
2018-10-28 10:57:53 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,15 +1,15 @@
-token = "590402522:AAGpO9DGyJjJ968Jsyc6CGSx_m1feZ9dYcY" -- prime bot token
-sudo_add = 434418436 -- prime full sudo
+token = "590402522:AAGpO9DGyJjJ968Jsyc6CGSx_m1feZ9dYcY" -- sasanApi token | get from @botfather
+sudo_add = 434418436 -- full access sudo id | get from @userinfobot
 sudo_users = {
-  434418436, -- prime sudo's
+  434418436, -- full access sudo list 
 }
-botnamefa = 'SASAN pRiMe - ساسان پرایم'
-sudoid = '@MRlte'
-supportgap = 'rull'
-channel = '@goldtabchi' 
-channel1 = 'goldtabchi' -- without @
-pvresan = '@pv_sasanbot'
+botnamefa = 'sasanAPI' -- name of source and help parts
+sudoid = '@MRlte' -- the sudo username 
+supportgap = 'rull' -- the sup gp invite link
+channel = '@goldtabchi' -- the channel of bot
+channel1 = 'goldtabchi' -- the channel of bot without @
+pvresan = '@pv_sasanbot' -- the sudo pvresan bot
 hypername = 'SASAN - NEWS'
-botnameen = 'SASAN - PRIME'
+botnameen = 'SASAN - API'
 
 
